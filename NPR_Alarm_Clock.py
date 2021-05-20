28d67790b41189ac3fbf78d61bd78daed7994e47
Brendan Inglis <79180847+bji219@users.noreply.github.com>
2021-05-19 22:15:09 -0400

    Update NPR_Alarm_Clock.py

--- NPR_Alarm_Clock.py
+++ NPR_Alarm_Clock.py
@@ -17,7 +17,6 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
 # Using Chrome to access web without GUI
-# driver = webdriver.Chrome(executable_path='./chromedriver')  # need to be on VPN???
 driver = webdriver.Chrome(executable_path='./chromedriver', options=options)  # headless (no window)
 
 # Open the website
@@ -25,7 +24,6 @@ driver.get('https://www.npr.org/programs/morning-edition/')
 
 # Small wait to ensure Listen Live is available
 delay = 2  # seconds
-#myElem = WebDriverWait(driver, delay)
 
 try:
     myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'player-item-stream')))
@@ -33,23 +31,18 @@ try:
 except TimeoutException:
     print("Loading took too much time!")  # Still works even though it is timing out
 
-# 'Listen Live' Button
+# Click 'Listen Live' Button
 init_button = driver.find_element_by_class_name('btn-live-radio')
 init_button.click()
 
 # Insert a brief wait so the screen can expand and become findable
 myElem = WebDriverWait(driver, delay)
-#try:
-    #myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'player-item-stream')))
-    #print("Page is ready!")
-#except TimeoutException:
-    #print("Loading took too much time!")
 
-# Live radio play button
+# Click Live radio play button
 play_button = driver.find_element_by_class_name('player-item-stream')
 play_button.click()
 
 # Close and quit driver after 50 minutes (when morning edition is over)
 time.sleep(3000)  # seconds- leave it up for 50 minutes so 3000
 driver.close()
-driver.quit()  # Current Issues: Only works with VPN Need to make a CRON job
+driver.quit()
